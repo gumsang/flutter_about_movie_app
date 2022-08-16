c3386780d4d6ccd762b61536292f3511d69326e0
eunsang.kim <gomuzom@naver.com>
2022-08-16 15:01:01 +0900

    claer: git cache

--- lib/main_screen/api/weather_api.dart
+++ lib/main_screen/api/weather_api.dart
@@ -2,10 +2,12 @@ import 'dart:convert';
 import 'package:flutter_about_movie_app/main_screen/model/model.dart';
 import 'package:http/http.dart' as http;
 
+import '../../api_keys/api_keys.dart';
+
 class WeatherApi {
   Future<Movie> getMovieList() async {
     Uri url = Uri.parse(
-        'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1');
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=$ssam_api_keys&language=ko-KR&page=1');
 
     http.Response response = await http.get(url);
     String jsonString = response.body;
