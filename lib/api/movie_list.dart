aae0709e7e35d8151ebad5e6b36aa3e25df6b9be
eunsang.kim <gomuzom@naver.com>
2022-08-16 17:49:00 +0900

    feat: detailmovie api 위치 변경

--- lib/api/movie_list.dart
+++ lib/api/movie_list.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter_about_movie_app/component/model.dart';
-import 'package:flutter_about_movie_app/detail_screen/model/detail_movie.dart';
 import 'package:http/http.dart' as http;
 
 import '../../api_keys/api_keys.dart';
@@ -16,16 +15,5 @@ class MovieApi {
     Map<String, dynamic> json = jsonDecode(jsonString);
     Iterable result = json['results'];
     return result.map((e) => AboutMovie.fromJson(e)).toList();
-  }
-
-  Future<DetailMovie> getMovieDetail(String query) async {
-    Uri url = Uri.parse(
-        'https://api.themoviedb.org/3/movie/$query?api_key=$ssam_api_keys');
-
-    http.Response response = await http.get(url);
-    String jsonString = response.body;
-
-    Map<String, dynamic> json = jsonDecode(jsonString);
-    return DetailMovie.fromJson(json);
   }
 }
